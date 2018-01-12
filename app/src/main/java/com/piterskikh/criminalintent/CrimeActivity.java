ef3685362f81fca8e0d7697b363e980ef286fe60
piterskikh <sergei.piterskih@exubit.com>
2018-01-12 16:31:35 +0800

    Chapter8

--- app/src/main/java/com/piterskikh/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/piterskikh/criminalintent/CrimeActivity.java
@@ -7,24 +7,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-public class CrimeActivity extends AppCompatActivity {
+public class CrimeActivity extends SingleFragmentActivity {
 
     private final String TAG = "CRIMEACTIVITY";
 
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime);
-
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-
-        if (fragment == null) {
-            Log.d(TAG, "Fragment added!");
-            fragment = new CrimeFragment();
-            fm.beginTransaction()
-            .add(R.id.fragment_container, fragment)
-            .commit();
-        }
+    protected Fragment createFragment() {
+        return new CrimeFragment();
     }
 }
---
+++ app/src/main/java/com/piterskikh/criminalintent/CrimeListActivity.java
@@ -0,0 +1,15 @@
+package com.piterskikh.criminalintent;
+
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+    private final String TAG = "CRIMELISTACTIVITY";
+
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
